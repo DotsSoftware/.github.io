ae9c2848a76d727d08c0636da80fe1b11c3152bd
DotsSoftware <dotsconsultants@gmail.com>
2025-01-16 17:56:57 +0200

    Updates
    -Page loading without loggout, Clients and Consultants
    -Dashboard Ui

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "3f9394a80ea2eebe5a73e45378f368a7",
+const RESOURCES = {"flutter_bootstrap.js": "31ea0af84f2818e2ac72827a2b4e991b",
 "version.json": "1dd229f57967c4f13ecfcec9b092a0af",
 "index.html": "3b63cdff4c38245d7c4b5d17fc9b397a",
 "/": "3b63cdff4c38245d7c4b5d17fc9b397a",
-"main.dart.js": "8296f7ec2f23e2fdbba481663f199a6b",
+"main.dart.js": "317c13adb25954bf15dc31d3cc33ed64",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -22,7 +22,7 @@ const RESOURCES = {"flutter_bootstrap.js": "3f9394a80ea2eebe5a73e45378f368a7",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "1cec29c6a0a66289aadbe7c0caf3f38e",
-"assets/fonts/MaterialIcons-Regular.otf": "54a53fc8f4e4a8b2498858db4c2915c9",
+"assets/fonts/MaterialIcons-Regular.otf": "fc8d4a38803ece7b1dc955cb996b3187",
 "assets/assets/images/logo.png": "13f8ef6746d0153234e448ee9cccf273",
 "canvaskit/skwasm.js": "ac0f73826b925320a1e9b0d3fd7da61c",
 "canvaskit/skwasm.js.symbols": "96263e00e3c9bd9cd878ead867c04f3c",
